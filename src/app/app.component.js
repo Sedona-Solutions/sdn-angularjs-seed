3ee5c01b3892f29854346df5631fd81ea03bcd0f
Noel Mace <nmace@sedona.fr>
2016-05-09 15:32:22 +0200

    clean main module config

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -13,30 +13,11 @@ import { Module } from 'decorators/Module';
 import { Component } from 'decorators/Component';
 import { lazyLoadConfig } from 'decorators/utils/routes';
 import { Routes } from 'decorators/Routes';
-
-appModuleConfig.$inject = ['$urlRouterProvider', '$locationProvider', '$compileProvider', '$logProvider', '$httpProvider', '$ocLazyLoadProvider'];
-export function appModuleConfig($urlRouterProvider, $locationProvider, $compileProvider, $logProvider, $httpProvider, $ocLazyLoadProvider) {
-    $locationProvider.html5Mode({
-        enabled: true,
-        requireBase: false
-    });
-    $httpProvider.useApplyAsync(true);
-    $urlRouterProvider.otherwise('/dashboards');
-
-    if (window.prod) {
-        $logProvider.debugEnabled(false);
-        // http://ng-perf.com/2014/10/24/simple-trick-to-speed-up-your-angularjs-app-load-time/
-        $compileProvider.debugInfoEnabled(false);
-    }
-
-    $ocLazyLoadProvider.config({
-        debug: true
-    });
-};
+import { DashboardComponent } from 'app/dashboard/dashboards.component';
 
 @Routes([
     {name: 'admin', path: '/admin', lazy: true, component: 'app/admin/admin.component'},
-    {name: 'dashboards', path: '/dashboards', lazy: true, component: 'app/dashboard/dashboards.component'},
+    {name: 'dashboards', path: '/dashboards', component: DashboardComponent, useAsDefault: true},
     {name: 'forms', path: '/forms', lazy: true, component: 'app/forms/demo-forms.component'}
 ])
 @Module({
@@ -47,9 +28,12 @@ export function appModuleConfig($urlRouterProvider, $locationProvider, $compileP
         'ct.ui.router.extras',
         template.name
     ],
-    configs: [
-        appModuleConfig
-    ]
+    main: true,
+    html5mode: {
+        enabled: true,
+        requireBase: false
+    },
+    debug: true
 })
 @Component({
     selector: 'sdn-seed',
@@ -60,6 +44,3 @@ export class AppComponent {
     /* @ngInject */
     constructor() {}
 }
-
-let appModule = angular.module('sdnSeed');
-appModule.config(lazyLoadConfig);
