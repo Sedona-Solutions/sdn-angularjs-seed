4156c75ee56b344210f649010a5c9204d3468134
Noel Mace <nmace@sedona.fr>
2016-06-06 18:55:12 +0200

    fix bad gulp paths

--- build/paths.js
+++ build/paths.js
@@ -1,9 +1,9 @@
 module.exports = {
     source: ['src/app/{,*/,*/*/}*.js', 'src/common/{,*/,*/*/}*.js'],
-    html: ['src/app/{,*/,*/*/}*.html', 'index.html'],
+    html: ['src/app/{,*/,*/*/}*.html'],
     json: ['src/app/{,*/,*/*/}/*.json', 'src/common/{,*/,*/*/}/*.json'],
-    templates: 'src/app/{,*/,*/*/}*.html',
-    less: ['src/app/{,*/,*/*/}*.less'],
+    templates: 'src/**/*.tpl.html',
+    less: ['src/{,*/,*/*/,*/*/*/}*.less'],
     output: 'dist/',
     outputCss: 'dist/**/*.css',
     tests: 'test/e2e/**/*.spec.js',
