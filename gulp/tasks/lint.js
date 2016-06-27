a2d5b1a90cdaccf23e2a6e4434d78bc34284a5b0
Sylvain BIGONNEAU <sbigonneau@sedona.fr>
2016-06-27 16:15:01 +0200

    NEW : eslint + task

--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var stylish = require('jshint-stylish');
+var eslint = require('gulp-eslint');
 var paths = require('../paths');
 
 gulp.task('lint', function () {
   return gulp.src(paths.source)
-    .pipe(jshint())
-    .pipe(jshint.reporter(stylish));
+    .pipe(eslint())
+    .pipe(eslint.format());
+});
+
+gulp.task('lint-nonblocking', function () {
+  return gulp.src(paths.source)
+    .pipe(eslint())
+    .pipe(eslint.format());
 });
