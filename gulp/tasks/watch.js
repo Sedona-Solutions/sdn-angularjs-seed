6db267d09e4e441afcba41f25aca96217d400b03
Sylvain BIGONNEAU <sbigonneau@sedona.fr>
2016-07-12 10:42:48 +0200

    ENH : watch config files and libs

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -8,6 +8,7 @@ function changed(event) {
 
 gulp.task('watch', ['build'], function () {
   gulp.watch([ paths.source ], [ 'es6', 'lint', browserSync.reload ]).on('change', changed);
+  gulp.watch([ paths.systemConfig, paths.config, paths.jspm ], [ 'move', browserSync.reload ]).on('change', changed);
   gulp.watch([ paths.html ], [ 'html', browserSync.reload ]).on('change', changed);
   gulp.watch([ paths.less ], [ 'less' ]).on('change', changed);
 });
