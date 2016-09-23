43805fb5aad800fa9726cfce09a0a28a9d0a9e5b
Noel Mace <nmace@sedona.fr>
2016-09-23 15:26:07 +0200

    fix gulp issues on "release"
    The version of traceur used by connect-prism was is conflict with the
    one used by systemjs-builder. Import connect-prism, and use it only at
    execution time, instead of tasks definition time, fix the "Map on
    undefined" issue when running "npm run release".
    
    The missing lodash dev dependency was added at the same time.

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -1,76 +1,78 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var connect = require('connect');
-var prism = require('connect-prism');
 var config = require('../config');
 var http = require('http');
 var proxy = require('http-proxy-middleware');
 
-// create and run a new connect-prism server
-// a proxy for backend requests with advanced mocking functions
-// prismMode : 'record' | 'mock' | 'proxy' | 'mockrecord'
-// cf. https://github.com/seglo/connect-prism#mode
-function prismInit(prismMode) {
-    prismMode = prismMode || 'proxy';
-
-    console.info('prism mode : ' + prismMode);
-
-    // connect-prism middleware configuration
-    // cf. https://github.com/seglo/connect-prism#options
-    var prismConfig = {
-        name: 'api',
-        mode: prismMode,
-        context: config.getConfig('api').basepath,
-        host: config.getConfig('api').host,
-        port: config.getConfig('api').port,
-        delay: 'auto',
-        rewrite: {
-        }
-    };
-
-    // define the prism middleware
-    prism.create(prismConfig);
-
-    // define the connect http server, and use the prism middleware to handle backend requests
-    var app = connect()
-        .use(prism.middleware);
-
-    // create the http server and run it
-    http.createServer(app).listen(config.getConfig('prism').port);
-}
-
-// create and run the browser sync server
-// an http server with watch and reload functions used for serving the angular project itself
-// cf. https://www.browsersync.io/docs/gulp/
-function browserSyncInit(done) {
-
-    var pathRewrite = {};
-    pathRewrite[`^${config.getConfig('api').route}`] = config.getConfig('api').basepath;
-
-    // configuration for the browser-sync http-proxy-middleware
-    // used to redirect backend request (ex: /api) to the connect(-prism) server
-    // cf. https://github.com/chimurai/http-proxy-middleware
-    var apiProxy = proxy(config.getConfig('api').route, {
-        target: 'http://localhost:' + config.getConfig('prism').port,
-        changeOrigin: true, // for vhosted sites, changes host header to match to target's host
-        pathRewrite: pathRewrite,
-        logLevel: 'debug'
-    });
-    
-    // create and run the http server
-    browserSync({
-        open: false,
-        port: 9000,
-        server: {
-            baseDir: ['target/tmp/'],
-            middleware: [ apiProxy ]
-        }
-    }, done);
-}
-
 // build and run the project for development purpose thanks to browsersync
 // the 'watch' task permit to auto-rebuild and reload all modified code in real time
 gulp.task('serve', ['watch'], function (done) {
+
+    var prism = require('connect-prism');
+
+    // create and run a new connect-prism server
+    // a proxy for backend requests with advanced mocking functions
+    // prismMode : 'record' | 'mock' | 'proxy' | 'mockrecord'
+    // cf. https://github.com/seglo/connect-prism#mode
+    function prismInit(prismMode) {
+        prismMode = prismMode || 'proxy';
+
+        console.info('prism mode : ' + prismMode);
+
+        // connect-prism middleware configuration
+        // cf. https://github.com/seglo/connect-prism#options
+        var prismConfig = {
+            name: 'api',
+            mode: prismMode,
+            context: config.getConfig('api').basepath,
+            host: config.getConfig('api').host,
+            port: config.getConfig('api').port,
+            delay: 'auto',
+            rewrite: {
+            }
+        };
+
+        // define the prism middleware
+        prism.create(prismConfig);
+
+        // define the connect http server, and use the prism middleware to handle backend requests
+        var app = connect()
+            .use(prism.middleware);
+
+        // create the http server and run it
+        http.createServer(app).listen(config.getConfig('prism').port);
+    }
+
+    // create and run the browser sync server
+    // an http server with watch and reload functions used for serving the angular project itself
+    // cf. https://www.browsersync.io/docs/gulp/
+    function browserSyncInit(done) {
+
+        var pathRewrite = {};
+        pathRewrite[`^${config.getConfig('api').route}`] = config.getConfig('api').basepath;
+
+        // configuration for the browser-sync http-proxy-middleware
+        // used to redirect backend request (ex: /api) to the connect(-prism) server
+        // cf. https://github.com/chimurai/http-proxy-middleware
+        var apiProxy = proxy(config.getConfig('api').route, {
+            target: 'http://localhost:' + config.getConfig('prism').port,
+            changeOrigin: true, // for vhosted sites, changes host header to match to target's host
+            pathRewrite: pathRewrite,
+            logLevel: 'debug'
+        });
+        
+        // create and run the http server
+        browserSync({
+            open: false,
+            port: 9000,
+            server: {
+                baseDir: ['target/tmp/'],
+                middleware: [ apiProxy ]
+            }
+        }, done);
+    }
+
     prismInit(config.getConfig('prism').mode);
     browserSyncInit(done);
 });
